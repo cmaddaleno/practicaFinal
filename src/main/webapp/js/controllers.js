201130091780e1cf16a270561ab6498c9ea61103
Victor Orozco <vorozco@nabenik.com>
2017-09-05 09:27:35 -0600

    Filter sample implementation

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -8,11 +8,16 @@ angular.module('mpApp.controllers',
 'mpApp.directives'])
 .controller('demoDirectivesController', function($scope) {
     $scope.staticjvmlanguages = [
-        {name:'Java', homepage: 'https://go.java', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/419px-Java_programming_language_logo.svg.png' },
-        {name:'Kotlin', homepage: 'https://kotlinlang.org/', logo: 'https://kotlinlang.org/assets/images/open-graph/kotlin_250x250.png' },
-        {name:'Ceylon', homepage: 'https://ceylon-lang.org/', logo: '' },
-        {name:'Clojure', homepage: 'https://clojure.org/', logo: '' },
-        {name:'Scala', homepage: 'https://www.scala-lang.org/', logo: '' }];
+        {name:'Java', homepage: 'https://go.java', birthday: new Date(1995, 05, 23, 01, 01, 01),
+            logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/419px-Java_programming_language_logo.svg.png' },
+        {name:'Kotlin', homepage: 'https://kotlinlang.org/', birthday: new Date(2011, 05, 23, 01, 01, 01),
+            logo: 'https://kotlinlang.org/assets/images/open-graph/kotlin_250x250.png' },
+        {name:'Ceylon', homepage: 'https://ceylon-lang.org/', birthday: new Date(2011, 05, 23, 01, 01, 01),
+            logo: '' },
+        {name:'Clojure', homepage: 'https://clojure.org/', birthday: new Date(2007, 05, 23, 01, 01, 01),
+            logo: '' },
+        {name:'Scala', homepage: 'https://www.scala-lang.org/', birthday: new Date(2004, 01, 20, 01, 01, 01),
+            logo: '' }];
         
     $scope.dynamicjvmlanguages = ['JRuby', 'Groovy', 'Jyton', 'Mirah', 'JavaScript'];
 

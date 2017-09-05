614f5986c536a1245d38aca76cb054a3724b7e4e
Victor Orozco <vorozco@nabenik.com>
2017-09-05 08:33:36 -0600

    Adding directive with dom restriction and manipulation

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -3,7 +3,13 @@ var module = angular.module('mpApp.directives', []);
 
 module.directive('mpDirective', function() {
     return {
-    templateUrl: 'view/language.html'
+        restrict: 'EA', //E = element, A = attribute, C = class, M = comment
+        templateUrl: 'view/language.html',
+        link: function ($scope, element, attrs) {
+            element.bind('click', function () {
+                element.html('Nombre: ' + $scope.item.name );
+            });
+        }
     };
 });
 

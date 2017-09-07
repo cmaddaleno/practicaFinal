5f0a5b915bc4dfbf930f11b421a8c564bbec3d05
Víctor Orozco <vorozco@nabenik.com>
2017-09-07 14:58:54 -0600

    Adding custom watch

--- src/main/webapp/app/posts/post-controller.js
+++ src/main/webapp/app/posts/post-controller.js
@@ -2,17 +2,27 @@
 var module = angular.module('mpApp.controllers');
 
 
-module.controller('postController', function($scope, $log, $http) {
+module.controller('postController', function($scope, $log) {
     
-    this.save = function(){
+    var pc = this;
+    
+    pc.save = function(){
         $log.warn('save');
         
     };
     
-    this.cancel = function(){
+    pc.cancel = function(){
         $log.warn('cancel');
     };
     
-    this.post = {};
+    pc.post = {'title':""};
+    pc.time = new Date();
+
+    //$scope.$watch('post.title', ...);
+    $scope.$watch(function() { return pc.post.title; }, function(newValue, oldValue) {
+        $log.info('changing post. Old value ' + oldValue + ' New value ' + newValue);
+    });
+    
+
 
 });

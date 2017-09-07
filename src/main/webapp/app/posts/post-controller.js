e2a23a5c5a0bffa60105178d8f19b175c778fb29
Victor Orozco <vorozco@nabenik.com>
2017-09-07 10:34:40 -0600

    Implementing regular syntax

--- src/main/webapp/app/posts/post-controller.js
+++ src/main/webapp/app/posts/post-controller.js
@@ -5,11 +5,12 @@ var module = angular.module('mpApp.controllers');
 module.controller('postController', function($scope, $log, $http) {
     
     $scope.save = function(){
+        $log.warn('save');
         
     };
     
     $scope.cancel = function(){
-        
+        $log.warn('cancel');
     };
 
 });

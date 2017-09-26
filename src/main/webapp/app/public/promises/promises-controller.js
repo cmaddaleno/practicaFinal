7be4d969bdbf697a325770e64806151736bec9bc
Víctor Orozco <vorozco@nabenik.com>
2017-09-26 12:27:29 -0600

    Multiple promises implementation

--- src/main/webapp/app/public/promises/promises-controller.js
+++ src/main/webapp/app/public/promises/promises-controller.js
@@ -2,7 +2,7 @@
 var module = angular.module('mpApp.public');
 
 
-module.controller('promisesController', function ($scope, $log, $http, helloService, comm, postResource) {
+module.controller('promisesController', function ($scope, $log, $http, $q, helloService, comm, postResource) {
 	
 	  $scope.hello = "";
 	  $scope.serverHello = "";
@@ -10,21 +10,34 @@ module.controller('promisesController', function ($scope, $log, $http, helloServ
 
 	  //  Promise chaining
 	  $scope.testPromise = function() {
-		  helloService.getHello()
-	      .then(function(result) {
-	        $scope.hello = result;
-	        return $http.get(comm.url + '/hello');
-	      })
-	      .then(function(result) {
-		        $scope.serverHello = result.data;
-		        return postResource.queryAll({"max":100}).$promise;
-		   })
-		   .then(function(result) {
-			   $scope.posts = result;
-		   })
-	      .catch(function(error) {
-		    console.log("An error occured: " + error);
-		  });
+//		  helloService.getHello()
+//	      .then(function(result) {
+//	        $scope.hello = result;
+//	        return $http.get(comm.url + '/hello');
+//	      })
+//	      .then(function(result) {
+//		        $scope.serverHello = result.data;
+//		        return ;
+//		   })
+//		   .then(function(result) {
+//			   $scope.posts = result;
+//		   })
+//	      .catch(function(error) {
+//		    console.log("An error occured: " + error);
+//		  });
+		  
+		  var allPromise = $q.all([  
+			  	helloService.getHello(),
+			  	$http.get(comm.url + '/hello'),
+			  	postResource.queryAll({"max":100}).$promise
+			]);
+
+			allPromise.then(function(values) {  
+			    $scope.hello = values[0];
+			    $scope.serverHello = values[1].data;
+			    $scope.posts = values[2];
+			    $log.info('promises done');
+			});
 	  };
     
 });
